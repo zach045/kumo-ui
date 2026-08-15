352d9509d864d72d64d837e4f8e385ef7d304536
Zachariah Reid <85593658+zach045@users.noreply.github.com>
2026-08-15 11:43:34 -0400

    Expand Kumo homepage and refine navigation

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,6 +2,10 @@
 import MainNav from '../components/layout/MainNav.vue';
 import MobileNav from '../components/layout/MobileNav.vue';
 import Hero from '../components/home/Hero.vue';
+import FeaturesSection from '../components/home/FeaturesSection.vue';
+import HowItWorks from '../components/home/HowItWorks.vue';
+import HomeCta from '../components/home/HomeCta.vue';
+import AppFooter from '../components/layout/AppFooter.vue';
 </script>
 
 <template>
@@ -9,5 +13,9 @@ import Hero from '../components/home/Hero.vue';
         <MainNav class="hidden md:flex" />
         <MobileNav class="md:hidden" />
         <Hero />
+        <FeaturesSection />
+        <HowItWorks />
+        <HomeCta />
+        <AppFooter />
     </main>
 </template>
