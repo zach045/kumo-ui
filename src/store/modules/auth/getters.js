7fc6fa19b1b42759aea868b18021155c57f871a1
Zachariah Reid <85593658+zach045@users.noreply.github.com>
2026-08-15 20:36:27 -0400

    Remove browser-readable authentication state

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,6 +1,5 @@
 export default {
-    isAuthenticated: state => !!state.token,
-    getUser: state => state.user,
-    getAuthError: state => state.authError,
-    getToken: state => state.token
-}
+  isAuthenticated: (state) => Boolean(state.user),
+  getUser: (state) => state.user,
+  getAuthError: (state) => state.authError,
+};
