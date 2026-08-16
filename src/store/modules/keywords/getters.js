fe928750dda5fc94b3a3282e29a6c1c3334e2c2c
Zachariah Reid <85593658+zach045@users.noreply.github.com>
2026-08-16 17:59:39 -0400

    Expose keyword analysis getter

--- src/store/modules/keywords/getters.js
+++ src/store/modules/keywords/getters.js
@@ -1,5 +1,6 @@
 export default {
   getKeywords: (state) => state.keywords,
+  getKeywordAnalysis: (state) => (id) => state.analyses[id] || null,
   getKeywordsLoading: (state) => state.isLoading,
   getKeywordsError: (state) => state.error,
 };
